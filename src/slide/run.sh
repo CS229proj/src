92300ff9bd2b76c0230a132fc5d348b786dede2e
Emrah Budur <emrah.budur@yahoo.com>
2015-12-23 18:32:09 -0800

    Implemented - Save model feature

--- src/slide/run.sh
+++ src/slide/run.sh
@@ -1,1 +1,1 @@
-THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python Slide_Test_Bed.py
+nohup THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python Slide_Test_Bed.py > slide.out 2> slide.err < /dev/null &
