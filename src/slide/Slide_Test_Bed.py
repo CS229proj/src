6a2722c0ad27e13733a1fb2154b45da280f068ac
Emrah Budur <emrah.budur@yahoo.com>
2015-12-25 07:30:33 -0800

    Exaustive parameter testing implemented

--- src/slide/Slide_Test_Bed.py
+++ src/slide/Slide_Test_Bed.py
@@ -34,29 +34,35 @@ def parameter_testing(train_file, test_file, gold_file):
     X_test_raw = test_data['text'].values
     gold_labels = Utils.get_y(gold_file)
 
-    param_num = 2
+    param_num = 4
     accuracy_list = []
     for i0 in xrange(param_num):
         predictor_list0 = [i0]
         overall_accuracy = calculate_accuracy(X_test_raw, gold_labels, slide, predictor_list0, False, True)
         accuracy_list.append((predictor_list0, overall_accuracy))
-        for i1 in [ x for x in xrange(param_num) if x > i0]:
+        for i1 in [ x for x in xrange(param_num) if x >= i0]:
             predictor_list1 = predictor_list0
             predictor_list1.append(i1)
             overall_accuracy = calculate_accuracy(X_test_raw, gold_labels, slide, predictor_list1, False, True)
             accuracy_list.append((predictor_list1, overall_accuracy))
 
-            for i2 in [ x for x in xrange(param_num) if x > i1]:
+            for i2 in [ x for x in xrange(param_num) if x >= i1]:
                 predictor_list2 = predictor_list1
                 predictor_list2.append(i2)
                 overall_accuracy = calculate_accuracy(X_test_raw, gold_labels, slide, predictor_list2, False, True)
                 accuracy_list.append((predictor_list2, overall_accuracy))
 
-                for i3 in [ x for x in xrange(param_num) if x > i2]:
+                for i3 in [ x for x in xrange(param_num) if x >= i2]:
                     predictor_list3 = predictor_list2
                     predictor_list3.append(i3)
                     overall_accuracy = calculate_accuracy(X_test_raw, gold_labels, slide, predictor_list3, False, True)
                     accuracy_list.append((predictor_list3, overall_accuracy))
+
+                    for i4 in [ x for x in xrange(param_num) if x >= i3]:
+                        predictor_list4 = predictor_list4
+                        predictor_list4.append(i4)
+                        overall_accuracy = calculate_accuracy(X_test_raw, gold_labels, slide, predictor_list4, False, True)
+                        accuracy_list.append((predictor_list4, overall_accuracy))
 
     plot_accuracy_list(accuracy_list)
 
@@ -66,7 +72,7 @@ def plot_accuracy_list(accuracy_list):
     x = xrange(len(accuracy_list))
     y = [tupple[1] for tupple in accuracy_list]
 
-    labels = [str(x) for x in [tupple[0] for tupple in accuracy_list]]
+    labels = [str(item) for item in [tupple[0] for tupple in accuracy_list]]
 
     print('labels', labels)
 
