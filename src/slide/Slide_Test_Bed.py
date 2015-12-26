ce5a1f7dd05faa6591a2f4919d741fd309945202
Emrah Budur <emrah.budur@yahoo.com>
2015-12-25 18:43:51 -0800

    Exaustive parameter testing implemented

--- src/slide/Slide_Test_Bed.py
+++ src/slide/Slide_Test_Bed.py
@@ -97,13 +97,11 @@ def plot_accuracy_list(accuracy_list):
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.set_ylim(0,1)
-    plt.plot(x,y, 'ro')
     plt.figure(figsize=(50,10))
-    plt.xlim(0, max(x))
-    plt.ylim(0.8, 1)
-
+    plt.plot(x,y, 'ro')
     ax.set_xticks(x)
+    ax.set_xlim(0, max(x))
+    ax.set_ylim(0.8, 1)
     ax.set_xticklabels(labels, rotation='vertical')
     #for i,j in zip(x,y):
     #    ax.annotate(str(i)+' , '+"{0:.2f}".format(round(j,2)),xy=(i,j))
